342bcb4cd9253d8b1d9327c553b3d165f95273b8
Pratik Ratadiya <32008483+prratadiya@users.noreply.github.com>
2018-04-20 07:42:06 +0530

    Update A1.cpp

--- A1.cpp
+++ A1.cpp
@@ -1,634 +1,400 @@
 #include<iostream>
 using namespace std;
+#include<stack>
+#include<queue>
+#include<utility>
+
 class node
 {
-  int data;
-  node* lchild;
-  node* rchild;
+	node *right;
+	node *left;
+	int info;
 public:
-  node()
-  {
-    data = 0;
-    lchild = NULL;
-    rchild = NULL;
-  }
-  node(int x)
-  {
-    data = x;
-    lchild = NULL;
-    rchild = NULL;
-  }
-  friend class tree;
+	node(){
+		right=NULL;
+		left=NULL;
+		info=0;
+}
+	friend class btree;
 };
 
-class tree
-{
-  node* root;
-public:
-  tree()
-  {
-    root = NULL;
-  }
-  void create(int x);
-  node* insert(node* t, int x);
-  void insertNode(int x);
-  void Postorder(node* p);
-  void displayPostorder();
-  void Preorder(node* p);
-  void displayPreorder();
-  void Inorder(node* p);
-  void displayInorder();
-  node* mirror(node*);
-  void callMirror();
-  int calHeight(node* t);
-  void height();
-  void postNonrecursive();
-  void preNonrecursive();
-  void inNonrecursive();
-  void deleteAll();
-  void deleteNode(node* p);
-  void countLeaf();
-  int returnLeaf(node* p, int cnt);
-  void countNode();
-  int returnNode(node* p);
-  void operator =(tree& obj)
-  {
-	  root = copy(obj.root);
-  }
-  node* copy(node* p)
-  {
-	  if(p == NULL)
-	  {
-		  return NULL;
-	  }
-	  node* a = new node();
-	  	a->data = p->data;
-	  	a->lchild = copy(p->lchild);
-	  	a->rchild = copy(p->rchild);
-	  	return a;
-  }
+class btree{
+	node *root;
+	queue<node*> f;
+	public:
+	btree(){
+		root=NULL;
+	}
+	void create();
+	node* createR();
+	void inordernonrec();
+	void postordernonrec();
+	void preordernonrec();
+	void inorderrec();
+	void inorder(node*);
+	void postorderrec();
+	void postorder(node*);
+	void preorderrec();
+	void preorder(node*);
+	int height();
+	int heightR(node*);
+	void leafinteriornode();
+	void delnode();
+	void del(node*);
+	node* mirror(node*);
+	node* getroot();
+	void setroot(node*);
+	void operator=(btree&);
+	node* copy(node*);
 };
-class stack
-{
-	int top;
-	node* t[40];
-public:
-	stack()
-{
-		top = -1;
-}
-	void push(node* a)
-	{
-		t[++top] = a;
-	}
-	node* pop()
-	{
-		return t[top--];
-	}
-	bool isempty()
-	{
-		if(top == -1)
-			return true;
-		else
-			return false;
-	}
-	friend class tree;
-};
-void tree::postNonrecursive()
-{
-	stack s;
-	node* temp = root;
-	node* flag = new node();
-	flag->data = -1;
-	do
-	{
-		while(temp!= NULL)
-		{
-			s.push(temp);
-			temp = temp->lchild;
-		}
-		temp = s.pop();
-		while(temp->data == -1 && !s.isempty())
-		{
-			temp = s.pop();
-			cout<<temp->data<<"\t";
-			if(s.isempty())
-				return;
-			temp = s.pop();
-		}
-		s.push(temp);
-		s.push(flag);
-		temp = temp->rchild;
-	}while(!s.isempty());
 
-}
-void tree::preNonrecursive()
-{
-	stack s;
-	node* temp = root;
-	while(true)
-	{
-		while(temp!=NULL)
-		{
-			s.push(temp);
-			cout<<temp->data<<"\t";
-			temp = temp->lchild;
-		}
-		if(s.isempty())
-		{
-			break;
-		}
-		temp = s.pop();
-		temp = temp->rchild;
+void btree::create(){
+	stack<node*> s;
+	if(root==NULL){
+		cout<<"Enter data for root:";
+		int temp;
+		cin>>temp;
+		node* new1;
+		new1=new node;
+		new1->info=temp;
+		root=new1;
+		s.push(new1);
 	}
+	while(!s.empty()){
+		node* temp=s.top();
+		s.pop();
+		cout<<"consider element "<<temp->info<<endl;
+		cout<<"Does this have left child:(y/n)";
+		char op;
+		int t;
+		cin>>op;
+		if(op=='y'){
+			cout<<"enter data for left:(y/n)";
+			cin>>t;
+			node* new1=new node;
+			new1->info=t;
+			temp->left=new1;
+			s.push(new1);
+		}
+		cout<<"Does this have right child:(y/n)";
+		cin>>op;
+		if(op=='y'){
+			cout<<"Enter data for right:";
+			cin>>t;
+			node* new1=new node;
+			new1->info=t;
+			temp->right=new1;
+			s.push(new1);
+		}
+	}
+}
+	
+node* btree::createR(){
+	int val,ch;
+	cout<<"Enter value of node: ";
+	cin>>val;
+	if(val==-1)
+		return NULL;
+	else{
+		node *p=new node;
+	    p->info=val;
+	   cout<<"Does "<<p->info<<"left child?(1-y)";
+	   cin>>ch;
+	   if(ch==1)
+		   p->left=createR();
+	   cout<<"Does "<<p->info<<"right child?(1-y)";
+	   cin>>ch;
+	   if(ch==1)
+	   	   p->right=createR();
+	   return p;
+	}
+}
+
+	void btree::inorder(node *a){
+		if(a!=NULL){
+			inorder(a->left);
+			cout<<a->info<<"\t";
+			inorder(a->right);
+		}
+	}
+
+	void btree::inordernonrec(){
+		stack <node*> s;
+		node *ptr=root;
+		while(1){	
+		while(ptr!=NULL){
+			s.push(ptr);
+			ptr=ptr->left;
+		}
+		if(s.empty())
+			return;
+		ptr=s.top();
+		s.pop();
+		cout<<ptr->info<<"\t";
+		ptr=ptr->right;
+		}
+	}
+
+	void btree::postorder(node *a){
+		if(a!=NULL){
+			postorder(a->left);
+			postorder(a->right);
+			cout<<a->info<<"\t";
+		}
+	}
+
+	void btree::postordernonrec(){
+	    stack <node*> s1,s2;
+	    s1.push(root);
+	    while(!s1.empty()){
+		    node* temp;
+		    temp=s1.top();
+		    s1.pop();
+		    s2.push(temp);
+
+		    if(temp->left!=NULL){
+			    s1.push(temp->left);
+		    }
+		    if(temp->right!=NULL)
+			    s1.push(temp->right);
+	    }
+	    while(!s2.empty()){
+		    node* temp=s2.top();
+		    cout<<temp->info<<"\t";
+		    s2.pop();
+	    }
+	}
+
+	void btree::preorder(node *a){
+		if(a!=NULL){
+			cout<<a->info<<"\t";
+			preorder(a->left);
+			preorder(a->right);	
+		}
+	}
+
+	void btree::preordernonrec(){
+		stack<node*> s;
+		node* ptr;
+		ptr=root;
+
+	    while(1){
+		    while(ptr!=NULL){
+			    cout<<ptr->info;
+			    s.push(ptr);
+			    ptr=ptr->left;
+		    }
+		    if(s.empty())
+			    return;
+		    ptr=s.top();
+		    s.pop();
+		    ptr=ptr->right;
+	    }
+	}
+		
+int btree::heightR(node *t){
+	if(t==NULL)
+		return -1;
+	return(max(heightR(t->left),heightR(t->right))+1);
+}
+
+	int btree::height(){
+		queue<node*> r;
+		if(root==NULL){
+			return 0;
+		}	
+		else{
+			r.push(root);
+			int h=0;
+			while(1){
+				int nc=r.size();
+				if(nc==0){
+					return h;
+				}
+				else{
+					h++;
+					while(nc>0){
+						node *temp=r.front();
+						r.pop();
+						if(temp->left!=NULL)
+							r.push(temp->left);
+						if(temp->right!=NULL)
+							r.push(temp->right);
+						nc--;
+					}
+				}
+			}
+		}
+	}
+
+	void btree::leafinteriornode(){
+	int l=0,in=0;
+	stack<node*> s;
+	node *temp;
+	s.push(root);
+	while(!s.empty()){
+		temp=s.top();
+		s.pop();
+		if(temp->left==NULL && temp->right==NULL){
+			l++;
+		}
+		else{
+			in++;
+		}
+		if(temp->right!=NULL)
+			s.push(temp->right);
+		if(temp->left!=NULL)
+			s.push(temp->left);
+	}
+	cout<<"Total leaf nodes:"<<l<<"\n"<<"Total internal nodes:"<<in;
+}
+	
+	void btree::del(node *ptr){
+		if(ptr!=NULL){
+			del(ptr->left);
+			del(ptr->right);
+			delete ptr;
+		}
+	}
+
+	node* btree::getroot(){
+		return root;
+	}
+
+    node* btree::mirror(node *rt){
+	    node* m_root=NULL;
+	    if(rt==NULL){
+		    return NULL;
+	    }
+	    m_root=new node;
+	    m_root->info=rt->info;
+	    m_root->left=mirror(rt->right);
+	    m_root->right=mirror(rt->left);
+	    return m_root;
+    }
+
+void btree::setroot(node *r){
+	root=r;
+}
+
+void btree::operator=(btree &b){
+	node *s=NULL;
+	s=copy(b.root);
+	setroot(s);
+	cout<<"Inorder traversal of tree:"<<endl;
+	inordernonrec();
 	cout<<"\n";
-}
-void tree::create(int x)
-{
-  node* p;
-  if(root == NULL)
-  {
-    p = new node(x);
-    root = p;
-  }
-  else
-  {
-    cout<<"\ntree root is already created\n";
-    insert(root, x);
-  }
-}
-void tree::insertNode(int x)
-{
-	insert(root, x);
-}
-node* tree::insert(node* t, int x)
-{
-  node* p;
-  char ans;
-  if(t == NULL)
-  {
-    p = new node(x);
-    return p;
-  }
-  else
-  {
-    cout<<"\ninsert on left or right?\n";
-    cout<<"\n l for inserting left and r for inserting right\n"<<t->data<<"\n";
-    cin>> ans;
-    if(ans == 'l')
-    {
-      t->lchild = insert(t->lchild, x);
-    }
-    else{
-      t->rchild = insert(t->rchild, x);
-    }
-  }
-  return t;
-}
-void tree::Postorder( node* p)
-{
-     if (p == NULL)
-        return;
-     Postorder(p->lchild);
 
-     Postorder(p->rchild);
+	cout<<"Preorder traversal of tree:"<<endl;
+	preordernonrec();
+	cout<<"\n";
 
-     cout<<p->data<<"\t";
-}
-void tree::displayPostorder()
-{
-	if(root == NULL)
-		cout<<"\nempty tree\n";
-  Postorder(root);
+	cout<<"Postorder traversal of tree:"<<endl;
+    postordernonrec();
+    cout<<"\n";
 }
 
-void tree::deleteAll()
-{
-	deleteNode(root);
-}
-void tree::deleteNode(node* p)
-{
-	if(p == NULL) return;
-
-	deleteNode(p->lchild);
-
-	deleteNode(p->rchild);
-
-	cout<<"\ndeleted node is:"<<p->data<<endl;
-	delete p;
-
-	root = NULL;
-}
-void tree::Preorder(node* p)
-{
-  if(p == NULL)
-  return;
-  cout<<p->data<<"\t";
-
-  Preorder(p->lchild);
-
-  Preorder(p->rchild);
-}
-void tree::displayPreorder()
-{
-	if(root == NULL)
-		cout<<"\nempty tree\n";
-  Preorder(root);
-}
-
-void tree::Inorder(node* p)
-{
-  if(p == NULL)
-  return;
-  Inorder(p->lchild);
-
-  cout<<p->data<<"\t";
-
-  Inorder(p->rchild);
-}
-void tree::displayInorder()
-{
-	if(root == NULL)
-		cout<<"\nempty tree\n";
-  Inorder(root);
-}
-node* tree::mirror(node* t)
-{
-	if(t == NULL)
-	{
+node* btree::copy(node *rt){
+	node* m_root=NULL;
+	if(rt==NULL){
 		return NULL;
 	}
-	node* a = new node();
-	a->data = t->data;
-	a->lchild = mirror(t->rchild);
-	a->rchild = mirror(t->lchild);
-	return a;
+	m_root=new node;
+	m_root->info=rt->info;
+	m_root->left=mirror(rt->left);
+	m_root->right=mirror(rt->right);
+
+	return m_root;
 }
-void tree::callMirror()
-{
-	node* m = mirror(root);
-	cout<<"\ninorder traversal of mirror image of the binary tree is :\n";
-	Inorder(m);
-}
-int tree::calHeight(node* t)
-{
-	if(t == NULL)
-		return 0;
-	if(t->lchild == NULL && t->rchild == NULL)
-		return 0;
-	int l = calHeight(t->lchild);
-	int r = calHeight(t->rchild);
-	if(l>r)
-		return (l+1);
-	else
-		return r+1;
-}
-void tree::height()
-{
-	int h = calHeight(root);
-	cout<<"\nthe height of the binary tree is:\t"<<h;
-}
-int tree::returnNode(node* t)
-{
-	if(t == NULL)
-			return 0;
-		if(t->lchild == NULL && t->rchild == NULL)
-			return 1;
-		int l = returnNode(t->lchild);
-		int r = returnNode(t->rchild);
-		return (l+r+1);
-}
-void tree::countNode()
-{
-	int count = returnNode(root);
-	cout<<"\nno of nodes in the tree is:"<<count<<endl;
-}
-int tree::returnLeaf(node*p, int cnt)
-{
-	int cnt1, cnt2;
-	if(p == NULL)
-		return 0;
-	if(p->lchild==NULL && p->rchild==NULL)
-	{
-		return (cnt+1);
+
+int main(){
+	btree a,b2,b3;
+	int op,num;
+	char op1;
+	b:
+	cout<<"enter the operation you want to perform"<<"\n";
+	cout<<"1.create\n2.inorder\n3.postorder\n4.preorder\n5.height\n";
+	cout<<"6.interior leaf nodes print\n7.delete node\n8.mirroring\n9.copying";
+	cin>>op;
+	switch(op){
+		case 1:cout<<"1.recursively\n2.non-recursively\n";
+			cin>>num;
+			switch(num){
+				case 1:node *r;
+					r=a.createR();
+					a.setroot(r);
+					break;
+				case 2:a.create();
+					break;
+			}
+			break;
+		case 2:cout<<"1.recursively\n2.non-recursively\n";
+			cin>>num;
+			switch(num){
+			case 1:a.inorder(a.getroot());
+				break;
+			case 2:a.inordernonrec();
+				break;
+			}
+			break;
+		case 3:cout<<"1.recursively\n2.non-recursively\n";
+			cin>>num;
+			switch(num){
+			case 1:a.postorder(a.getroot());
+				break;
+			case 2:a.postordernonrec();
+				break;
+			}
+			break;
+		case 4:cout<<"1.recursively\n2.non-recursively\n";
+			cin>>num;
+			switch(num){
+			case 1:a.preorder(a.getroot());
+				break;
+			case 2:a.preordernonrec();
+				break;
+			}
+			break;
+		case 5:cout<<"1.recursively\n2.non-recursively\n";
+			cin>>num;
+			int h;
+			switch(num){
+			case 1:h=a.heightR(a.getroot());
+				break;
+			case 2:h=a.height();
+				break;
+			}
+			cout<<"the height is:"<<h<<"\n";
+			break;
+		case 6:a.leafinteriornode();
+			break;
+		case 7:a.del(a.getroot());
+			break;
+		case 8:	cout<<"Mirror tree\n";
+				node *mr;
+				mr=a.mirror(a.getroot());
+				b2.setroot(mr);
+
+				cout<<"Inorder traversal of tree:"<<endl;
+				b2.inordernonrec();
+				cout<<"\n";
+
+ 			    cout<<"Preorder traversal of tree:"<<endl;
+				b2.preordernonrec();
+				cout<<"\n";
+
+	      		cout<<"Postorder traversal of tree:"<<endl;
+			    b2.postordernonrec();
+			    cout<<"\n";
+				break;
+		case 9:cout<<"Copy tree\n";
+				b3=a;
+				break;
 	}
-	else
-	{
-		cnt1 = returnLeaf(p->lchild, cnt1);
-		cnt2 = returnLeaf(p->rchild, cnt2);
+	cout<<"do you want to do more operations";
+	cin>>op1;
+	if(op1=='y'||op1=='Y'){
+		goto b;
 	}
-	return (cnt1+cnt2);
-
+	return 0;
 }
-void tree::countLeaf()
-{
-	int count = returnLeaf(root, 0);
-	cout<<"\nno of leaf nodes int the tree is:"<<count<<endl;
-}
-int main()
-{
-  tree t, r;
-  int i, e;
-  do
-  {
-	  cout<<"\n1.create binary tree\n2.insert \n3.postorder traversal\n4.preorder traversal\n5.inorder traversal\n6.mirror image(inorder traversal)\n7.height of tree\n8.postorder traversal(non-recursive)\n9.preorder traversal(non-recursive)\n10.delete tree\n11.copy tree\n12.count leaf node\n";
-	  cin>> i;
-	  switch (i)
-	  {
-	  case 1:
-		  cout<<"\ninsert data\n";
-		  cin>> e;
-		  t.create(e);
-		  break;
-	  case 2:
-		  cout<<"\ninsert data\n";
-		   cin>> e;
-		   t.insertNode(e);
-		   break;
-	  case 3:
-		  t.displayPostorder();
-		  break;
-	  case 4:
-		  t.displayPreorder();
-		  break;
-	  case 5:
-		  t.displayInorder();
-		  break;
-	  case 6:
-		  t.callMirror();
-		  break;
-	  case 7:
-		  t.height();
-		  break;
-	  case 8:
-		  t.postNonrecursive();
-		  break;
-	  case 9:
-		  t.preNonrecursive();
-		  break;
-	  case 10:
-		  t.deleteAll();
-		  break;
-	  case 11:
-		  r = t;
-		  cout<<"\ndisplaying copied tree using postorder traversal\n";
-		  r.displayPostorder();
-		  break;
-	  case 12:
-		  t.countLeaf();
-		  break;
-	  }
-	  cout<<"\nenter 1 to continue\n";
-	  cin>> e;
-  }while(e == 1);
-  return 0;
-
-}
-/*
-shreyas@shreyas:~$ cd 2416
-shreyas@shreyas:~/2416$ g++ A1.cpp
-shreyas@shreyas:~/2416$ ./a.out
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-1
-
-insert data
-2
-
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-2
-
-insert data
-3
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-2
-l
-
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-2
-
-insert data
-6
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-2
-r
-
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-2
-
-insert data
-8
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-2
-l
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-3
-l
-
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-2
-
-insert data
-12
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-2
-r
-
-insert on left or right?
-
- l for inserting left and r for inserting right
-6
-r
-
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-3
-8	3	12	6	2	
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-4
-2	3	8	6	12	
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-5
-8	3	2	6	12	
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-6
-
-inorder traversal of mirror image of the binary tree is :
-12	6	2	3	8	
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-7
-
-the height of the binary tree is:	2
-enter 1 to continue
-1
-
-1.create binary tree
-2.insert 
-3.postorder traversal
-4.preorder traversal
-5.inorder traversal
-6.mirror image(inorder traversal)
-7.height of tree
-8.postorder traversal(non-recursive)
-9.preorder traversal(non-recursive)
-10.delete tree
-11.copy tree
-12.count leaf node
-12
-
-no of leaf nodes int the tree is:3
-
-enter 1 to continue
-2
-*/
